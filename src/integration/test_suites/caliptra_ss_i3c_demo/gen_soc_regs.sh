df51b8958b9dbfbd6c11a1cf7ee8138b6fd207df
Caleb Whitehead <cwhitehead@microsoft.com>
2024-10-10 21:20:45 -0700

    Add external soc_ifc regs to addr map

--- src/integration/test_suites/caliptra_ss_i3c_demo/gen_soc_regs.sh
+++ src/integration/test_suites/caliptra_ss_i3c_demo/gen_soc_regs.sh
@@ -1,6 +1,9 @@
 python3 ${CALIPTRA_ROOT}/tools/scripts/reg_doc_gen.py \
 ${CALIPTRA_SS}/src/integration/test_suites/caliptra_ss_i3c_demo/soc_address_map.rdl \
-${I3C_ROOT}/src/rdl/registers.rdl
+${CALIPTRA_ROOT}/src/soc_ifc/rtl/mbox_csr.rdl \
+${CALIPTRA_ROOT}/src/soc_ifc/rtl/sha512_acc_csr.rdl \
+${CALIPTRA_ROOT}/src/soc_ifc/rtl/soc_ifc_doc.rdl \
+../../../../../i3c-core/src/rdl/registers.rdl
 
 #../../../../../i3c-core/src/rdl/base_registers.rdl \
 #../../../../../i3c-core/src/rdl/pio_registers.rdl \
